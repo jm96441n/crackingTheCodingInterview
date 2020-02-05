a8c5337ce9a9eed25e9a15765d2d53d408a2ba54
jm96441n <john@jmaguire.tech>
2020-02-04 20:29:37 -0500

    added code for checking without using additional DS

--- arrays_and_strings/IsUnique.java
+++ arrays_and_strings/IsUnique.java
@@ -2,6 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class IsUnique {
+  /*
+   * Runtime complexity = O(n) because we only have to iterate through the string onces
+   * and each hash lookup/insertion is O(1) time
+   * Space complexity = O(n^2) because we are essentially duplicating each character in the
+   * string when we store it in the hash
+  */
   private boolean isUniqueUsingHashMap(String testCase) {
     HashMap<Character, Integer> frequencies = new HashMap<Character, Integer>();
     boolean unique = true;
@@ -11,6 +17,21 @@ public class IsUnique {
         break;
       } else {
         frequencies.put(testCase.charAt(i), 1);
+      }
+    }
+    return unique;
+  }
+
+  private boolean isUniqueNoDS(String testCase) {
+    boolean unique = true;
+
+    for(int i = 0; i < testCase.length(); i++) {
+      char compare = testCase.charAt(i);
+      for(int j = i + 1; j < testCase.length(); j++) {
+        if (compare == testCase.charAt(j)) {
+          unique = false;
+          break;
+        }
       }
     }
     return unique;
@@ -30,6 +51,13 @@ public class IsUnique {
       } else {
         System.out.println("HM: String is not unique: " + testCase);
       }
+
+      if (checker.isUniqueNoDS(testCase)) {
+        System.out.println("DS: String is unique: " + testCase);
+      } else {
+        System.out.println("DS: String is not unique: " + testCase);
+      }
+      System.out.println();
     }
   }
 }
