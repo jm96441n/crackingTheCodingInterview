13fbabc53f005e9014a5198667dfe95200b850e3
jm96441n <jmaguire@mdsol.com>
2020-02-10 20:00:25 -0500

    added code for finding if permutation of palindrome

--- arrays_and_strings/PalindromePermutation.java
+++ arrays_and_strings/PalindromePermutation.java
@@ -3,8 +3,40 @@ import java.io.*;
 
 public class PalindromePermutation {
   private boolean check(String testStr) {
+    boolean palindrome = true;
+    char[] charArr = testStr.replace(" ", "").toLowerCase().toCharArray();
+    HashMap<Character, Integer> charCounts = new HashMap<Character, Integer>();
 
-    return true;
+    for (Character c : charArr) {
+      if (charCounts.containsKey(c)) {
+        charCounts.put(c, charCounts.get(c) + 1);
+      } else {
+        charCounts.put(c, 1);
+      }
+    }
+
+    // if the length of the word is even, then all letters should have an even count
+    // else if the length is odd all the letters except one should have an even count
+    if (charArr.length % 2 == 0) {
+      for(Integer value : charCounts.values()) {
+        if (value % 2 != 0) {
+          palindrome = false;
+          break;
+        }
+      }
+    } else {
+      boolean seenOdd = false;
+      for(Integer value : charCounts.values()) {
+        if (value % 2 != 0 && seenOdd) {
+          palindrome = false;
+          break;
+        } else if (value % 2 != 0) {
+          seenOdd = true;
+        }
+      }
+    }
+
+    return palindrome;
   }
 
   public static void main(String[] args) {
